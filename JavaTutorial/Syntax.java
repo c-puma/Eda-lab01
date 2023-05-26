8509c142fe0c728b8c1e7fa801910660e3b42c79
Cristhian Willians Puma Larico <cpumal@unsa.edu.pe>
2023-05-26 18:46:43 -0500

    Cuarto tema del manual de java Syntax

---
+++ JavaTutorial/Syntax.java
@@ -0,0 +1,12 @@
+package Lab01;
+
+public class Syntax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("En este apartado nos explica un poco la sintaxis del lenguage "
+						+ "como el uso de parentesis y comas");
+	}
+
+}
