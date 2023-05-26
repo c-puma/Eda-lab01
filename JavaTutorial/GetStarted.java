fadc38edbe6d58eea5b15432a8d4c1ca33bd6a90
Cristhian Willians Puma Larico <cpumal@unsa.edu.pe>
2023-05-26 18:42:49 -0500

    Cree un archivo .java para el tercer tema del manual de java GetStarted

---
+++ JavaTutorial/GetStarted.java
@@ -0,0 +1,11 @@
+package Lab01;
+
+public class GetStarted {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Es una introduccion a java");
+		System.out.println("Nos explica como podemos compilar y ejecutar nuestro codigo desde consola");	
+	}
+
+}
