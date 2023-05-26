c3d6d6c999907c5095aab1563306675301e44f50
Cristhian Willians Puma Larico <cpumal@unsa.edu.pe>
2023-05-26 18:50:51 -0500

    Quinto tema del manual de java Output

---
+++ JavaTutorial/Output.java
@@ -0,0 +1,19 @@
+package Lab01;
+
+public class Output {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Tipo String
+		System.out.println("Hello World!");
+		System.out.println("I am learning Java.");
+		System.out.println("It is awesome!");
+		//TipoNumeros
+		System.out.println(3);
+		System.out.println(358);
+		System.out.println(50000);
+		System.out.println("En este apartado nos enseña como el la salida por consola de strings y numeros");
+	}
+
+}
