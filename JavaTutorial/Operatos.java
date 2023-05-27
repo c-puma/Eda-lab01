82ee275ae874759b4dc8bef9c28acc7c35f1d3d0
Cristhian Willians Puma Larico <cpumal@unsa.edu.pe>
2023-05-26 19:18:07 -0500

    Decimo tema del manual de java Operators

---
+++ JavaTutorial/Operatos.java
@@ -0,0 +1,22 @@
+package Lab01;
+
+public class Operatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 100 + 50; //Operador de suma
+		int sum1 = 100 + 50;        // 150 (100 + 50)
+		int sum2 = sum1 + 250;      // 400 (150 + 250)
+		int sum3 = sum2 + sum2;     // 800 (400 + 400)
+		
+		int y = 100 - 50; //Operador de resta
+		int z = 100 * 50; //Operador de multiplicacion
+		int w = 100 / 50; //Operador de division
+		
+		int x1 = 10; //Operador de asignacion
+		x1 += 5;
+		
+		
+	}
+}
