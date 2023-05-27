c2956daf3e4822b017259021744d57f62d445788
Cristhian Willians Puma Larico <cpumal@unsa.edu.pe>
2023-05-26 20:09:51 -0500

    Quinta y ultimaa clase para JavaMethods Recursion

---
+++ JavaMethods/Recursion.java
@@ -0,0 +1,19 @@
+package Lab01;
+
+public class Recursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int result = sum(5, 10);
+		System.out.println(result);
+	}
+
+	public static int sum(int start, int end) {
+		if (end > start) {
+			return end + sum(start, end - 1);
+		} else {
+			return end;
+		}
+	}
+}
