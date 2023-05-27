95b56bf3708b78770e22f6716ebdbd8ad3b9cf6f
Cristhian Willians Puma Larico <cpumal@unsa.edu.pe>
2023-05-26 19:11:59 -0500

    Octavo tema del manual de java TiposdeDatos

---
+++ JavaTutorial/DataType.java
@@ -0,0 +1,21 @@
+package Lab01;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int myNum = 5;               // Almancena enteros
+		float myFloatNum = 5.99f;    // Almacena decimales y f al final
+		char myLetter = 'D';         // Almancena caracteres
+		boolean myBool = true;       // Almacena booleanos TRUE OR FALSE
+		String myText = "Hello";     // Almacena cadenas String
+		
+		System.out.print(myNum);
+		System.out.print(myFloatNum);
+		System.out.print(myLetter);
+		System.out.print(myBool);
+		System.out.print(myText);
+		
+	}
+}
