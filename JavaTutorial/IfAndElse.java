27b43aa5effa74a5cd8e542ea5ed4e5ecfd3d849
Cristhian Willians Puma Larico <cpumal@unsa.edu.pe>
2023-05-26 19:33:29 -0500

    DecimoCuarto tema del manual de java IfAndElse

---
+++ JavaTutorial/IfAndElse.java
@@ -0,0 +1,15 @@
+package Lab01;
+
+public class IfAndElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 20;
+	    int y = 18;
+	    if (x > y) {
+	        System.out.println("x is greater than y");
+	    }
+	   
+	}
+}
