4ffeba5117cdbf0cb69fbe2eab1bcd708d9171b2
Cristhian Willians Puma Larico <cpumal@unsa.edu.pe>
2023-05-26 19:38:09 -0500

    DecimoSeptimo tema del manual de java BucleFor

---
+++ JavaTutorial/ForLoop.java
@@ -0,0 +1,16 @@
+package Lab01;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+		}
+		
+		for (int i = 0; i <= 10; i = i + 2) {
+			System.out.println(i);
+		}
+	}
+}
