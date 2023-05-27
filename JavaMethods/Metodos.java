f09c6aa19af028ed77c4cbed52b5edb93e8e4373
Cristhian Willians Puma Larico <cpumal@unsa.edu.pe>
2023-05-26 19:57:39 -0500

    Primera clase de Metodo de JavaMethods

---
+++ JavaMethods/Metodos.java
@@ -0,0 +1,17 @@
+package Lab01;
+
+public class Metodos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String n = "Cristhian";
+		saludo(n);
+		
+	}
+	
+	public static void saludo(String name) {
+		System.out.print("Hola "+name);
+	}
+
+}
