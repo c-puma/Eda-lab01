cddb7432a9dd2d880822d7a840895c537f817a7c
Cristhian Willians Puma Larico <cpumal@unsa.edu.pe>
2023-05-26 19:31:20 -0500

    DecimoTercero tema del manual de java Booleanos

---
+++ JavaTutorial/Booleanos.java
@@ -0,0 +1,19 @@
+package Lab01;
+
+public class Booleanos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean isJavaFun = true;
+		boolean isFishTasty = false;
+		System.out.println(isJavaFun);     // Outputs true
+		System.out.println(isFishTasty);   // Outputs false
+		
+		int x = 10;
+		int y = 9;
+		System.out.println(x > y); // returns true, because 10 is higher than 9
+		
+	}
+
+}
