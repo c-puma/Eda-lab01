5e3663c8abcc2ef95b2bd0b4cd357183e655b530
Cristhian Willians Puma Larico <cpumal@unsa.edu.pe>
2023-05-26 19:36:36 -0500

    DecimoSexto tema del manual de java BucleWhile

---
+++ JavaTutorial/WhileLoop.java
@@ -0,0 +1,14 @@
+package Lab01;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 0;
+		while (i < 5) {
+		  System.out.println(i);
+		  i++;
+		}
+	}
+}
